1fd6aa51ebd0f652fb07aa835172b4ae4bb97d08
Spidey507 <miguelalveo@gmail.com>
2023-11-23 18:38:30 -0500

    password hash enabled for security

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -18,3 +18,19 @@ const userSchema = new mongoose.Schema({
         required: true, 
     }
 });
+
+// Hash pass
+userSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();        
+    } catch (error) {
+        next(error);
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
