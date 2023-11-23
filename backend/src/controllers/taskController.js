8aa47196e160f049cf9524355a821284f3461adf
Spidey507 <miguelalveo@gmail.com>
2023-11-23 16:35:51 -0500

    added function to get a function by its ID

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -24,4 +24,16 @@ exports.getAllTasks = async (req, res) => {
   }
 };
 
+// Get specific task by ID
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -4,5 +4,6 @@ const taskController = require('../controllers/taskController');
 
 router.post('/', taskController.createTask);
 router.get('/', taskController.getAllTasks);
+router.get('/:id', taskController.getTaskById);
 
 module.exports = router;
