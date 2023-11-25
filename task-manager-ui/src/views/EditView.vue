1be483d43f446727b347244f9430f59f70b4ead2
Spidey507 <miguelalveo@gmail.com>
2023-11-25 15:19:17 -0500

    error handling implemented for update function

--- task-manager-ui/src/views/EditView.vue
+++ task-manager-ui/src/views/EditView.vue
@@ -7,24 +7,30 @@
       <form class="flex-col content-center items-center justify-center">
         <div class="form-group">
           <label for="title" class="flex w-full justify-center text-white mb-2">Title</label>
-          <input v-model="editedTask.title" type="text" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
+          <input v-model="editedTask.title" type="text" required
+            class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
         </div>
 
         <div class="form-group">
           <label for="description" class="flex w-full justify-center text-white mb-2">Description</label>
-          <input v-model="editedTask.description" type="text" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
+          <input v-model="editedTask.description" type="text" required
+            class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
         </div>
 
         <div class="form-group">
           <label for="dueDate" class="flex w-full justify-center text-white mb-2">Due Date</label>
-          <input v-model="editedTask.dueDate" type="date" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center w-full" />
+          <input v-model="editedTask.dueDate" type="date" required
+            class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center w-full" />
         </div>
 
-        <!--
         <div class="error-group">
-          <div v-if="checkErrors()" class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          <div v-if="checkErrors()"
+            class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+            role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor" viewBox="0 0 20 20">
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
             <span class="sr-only">Info</span>
             <div>
@@ -32,11 +38,12 @@
             </div>
           </div>
         </div>
-        -->
 
         <div class="button-group mt-4 flex justify-between">
-          <button @click="updateTask" class="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded flex-grow">Update Task</button>
-          <button @click="cancelEdit" class="bg-red-500 hover:bg-red-600 text-white p-3 rounded flex-grow ml-2">Close</button>
+          <button @click="updateTask" class="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded flex-grow">Update
+            Task</button>
+          <button @click="cancelEdit"
+            class="bg-red-500 hover:bg-red-600 text-white p-3 rounded flex-grow ml-2">Close</button>
         </div>
       </form>
     </div>
@@ -44,30 +51,43 @@
 </template>
 
 
-  <script>
-  export default {
-    name: 'EditView',
-    mounted() {
-      this.getTask(this.$route.params.id);
+<script>
+export default {
+  name: 'EditView',
+  mounted() {
+    this.getTask(this.$route.params.id);
+  },
+  data() {
+    return {
+      editedTask: {
+        title: '',
+        description: '',
+        dueDate: '',
+        _id: '',
+      },
+      errorMessage: null,
+    }
+  },
+  watch: {
+    '$route.params.id'(newId) {
+      this.getTask(newId);
+    }
+  },
+  methods: {
+    checkErrors() {
+      return (this.errorMessage !== null);
     },
-    data() {
-      return {
-        editedTask: {
-          title: '',
-          description: '',
-          dueDate: '',
-          _id: ''
-        }
-      }
-    },
-    watch: {
-      '$route.params.id'(newId) {
-        this.getTask(newId);
-      }
-    },
-    methods: {
-      async updateTask() {
+    async updateTask() {
+      this.errorMessage = null;
+
+      try{
         const token = localStorage.getItem('token');
+        if (this.editedTask.title === null || this.editedTask.title === '') {
+          throw new Error('missing title');
+        }
+        if (this.editedTask.description === null || this.editedTask.description === '') {
+          throw new Error('missing description');
+        }
         const response = await fetch(`http://localhost:3000/api/tasks/${this.editedTask._id}`, {
           method: 'PUT',
           headers: {
@@ -82,34 +102,38 @@
           this.$router.push('/tasks');
         } else {
           // Manejar el error
+          
+          console.error('Error creating task:', response.statusText);
         }
-      },
-      async getTask(taskId) {
-        const token = localStorage.getItem('token');
-        const response = await fetch(`http://localhost:3000/api/tasks/${taskId}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        });
-        // Implement the logic to update the task
-        // Call an API endpoint or emit an event to update the task on the parent component
-        if (response.ok) {
-          const taskData = await response.json();
-          this.editedTask = taskData;
-        } else {
-          // Manejar el error
-        }
-      },
-      cancelEdit() {
-        this.$router.push('/tasks');
+      } catch(error){
+        console.error('Error updating task:', error);
+        this.errorMessage = error;
       }
+    },
+    async getTask(taskId) {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`http://localhost:3000/api/tasks/${taskId}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      // Implement the logic to update the task
+      // Call an API endpoint or emit an event to update the task on the parent component
+      if (response.ok) {
+        const taskData = await response.json();
+        this.editedTask = taskData;
+      } else {
+        // Manejar el error
+      }
+    },
+    cancelEdit() {
+      this.$router.push('/tasks');
     }
-  };
-  </script>
+  }
+};
+</script>
     
-  <style scoped>
-  /* Add your modal styles here */
-  </style>
+<style scoped>/* Add your modal styles here */</style>
     
