3559763aa1b4e3bdb715c7fa562003e8c045e25d
Spidey507 <miguelalveo@gmail.com>
2023-11-25 03:02:35 -0500

    added logout button

--- task-manager-ui/src/App.vue
+++ task-manager-ui/src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <button v-if="authStatus" @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
-}
+  data() {
+    return {
+      authStatus: this.isAuthenticated(),
+    };
+  },
+  watch: {
+    '$route': 'updateAuthStatus',
+  },
+  methods: {
+    isAuthenticated() {
+      return localStorage.getItem('token') !== null;
+    },
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/login');
+    },
+    updateAuthStatus() {
+      this.authStatus = this.isAuthenticated();
+    },
+  },
+};
 </script>
+
+
 
 <style>
 #app {
