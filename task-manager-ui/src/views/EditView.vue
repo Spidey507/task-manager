a298147359fdc25fb5880ba3137d9536e94d3325
Spidey507 <miguelalveo@gmail.com>
2023-11-25 14:50:28 -0500

    added styles to edit view

--- task-manager-ui/src/views/EditView.vue
+++ task-manager-ui/src/views/EditView.vue
@@ -1,21 +1,49 @@
 <template>
-    <div>
-      <h2>Edit Task</h2>
-      <p>Task ID: {{ $route.params.id }}</p>
-      <label for="title">Title:</label>
-      <input type="text" v-model="editedTask.title" required>
-  
-      <label for="description">Description:</label>
-      <input type="text" v-model="editedTask.description" required>
-  
-      <label for="dueDate">Due Date:</label>
-      <input type="date" v-model="editedTask.dueDate" required>
-  
-      <button @click="updateTask">Update Task</button>
-      <button @click="cancelEdit">Close</button>
+  <div class="bg-gray-800 min-h-screen flex items-center justify-center">
+    <div class="bg-gray-700 py-10 shadow-md rounded flex flex-col w-1/3 items-center">
+      <h1 class="text-white text-2xl mb-4">Edit Task</h1>
+      <p class="text-white mb-4">Task ID: {{ $route.params.id }}</p>
+
+      <form class="flex-col content-center items-center justify-center">
+        <div class="form-group">
+          <label for="title" class="flex w-full justify-center text-white mb-2">Title</label>
+          <input v-model="editedTask.title" type="text" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
+        </div>
+
+        <div class="form-group">
+          <label for="description" class="flex w-full justify-center text-white mb-2">Description</label>
+          <input v-model="editedTask.description" type="text" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center" />
+        </div>
+
+        <div class="form-group">
+          <label for="dueDate" class="flex w-full justify-center text-white mb-2">Due Date</label>
+          <input v-model="editedTask.dueDate" type="date" required class="flex mb-4 p-2 border rounded focus:outline-none focus:border-blue-500 text-center w-full" />
+        </div>
+
+        <!--
+        <div class="error-group">
+          <div v-if="checkErrors()" class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+              <span class="font-medium">{{ this.errorMessage }}</span>
+            </div>
+          </div>
+        </div>
+        -->
+
+        <div class="button-group mt-4 flex justify-between">
+          <button @click="updateTask" class="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded flex-grow">Update Task</button>
+          <button @click="cancelEdit" class="bg-red-500 hover:bg-red-600 text-white p-3 rounded flex-grow ml-2">Close</button>
+        </div>
+      </form>
     </div>
-  </template>
-    
+  </div>
+</template>
+
+
   <script>
   export default {
     name: 'EditView',
