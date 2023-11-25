28a2f58fae95c31d6dd44d9d118455b04b15271d
Spidey507 <miguelalveo@gmail.com>
2023-11-25 13:08:03 -0500

    fixed logout button

--- task-manager-ui/src/App.vue
+++ task-manager-ui/src/App.vue
@@ -3,8 +3,20 @@
     <head>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     </head>
-    <router-view></router-view>
-    <button v-if="authStatus" @click="logout">Logout</button>
+    <div class="bg-gray-800 h-screen flex flex-col">
+      <!-- Navbar -->
+      <div class="bg-gray-700 p-4 flex justify-between items-center">
+        <!-- Welcome message and logout button -->
+        <div class="text-white">
+          <!-- Welcome, {{ getUsername() }}! -->
+        </div>
+        <div class="flex items-center">
+          <!-- Logout button -->
+          <button v-if="authStatus" @click="logout" class="bg-red-700 text-white p-2 rounded">Logout</button>
+        </div>
+      </div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -36,6 +48,4 @@ export default {
 
 
 
-<style>
-
-</style>
+<style></style>
